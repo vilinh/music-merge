b8b232ab9cc9bd92cc7ee1665faac08d65456a48
vilinh <vilinhlvu@gmail.com>
2022-07-28 23:02:14 -0700

    active sidebar

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -5,23 +5,37 @@ export const Account = () => {
   return (
     <div className="account">
       <div className="account-sidebar">
-        <span>USER SETTINGS</span>
-        <span>my account</span>
-        <span>profile</span>
-        <span>change password</span>
-        <span>connections</span>
-        <span>APP SETTINGS</span>
-        <span>appearance</span>
+        <span className="subheading">USER SETTINGS</span>
+        <span className="link active">my account</span>
+        <span className="link">profile</span>
+        <span className="link">change password</span>
+        <span className="link">connections</span>
+        <span className="subheading">APP SETTINGS</span>
+        <span className="link">appearance</span>
       </div>
       <div className="account-main">
         <h3>My Account</h3>
+        <div className="profile-pic">
+          <img src="https://i.scdn.co/image/ab6775700000ee850e227e40152a057cad6fdf16" />
+        </div>
         <div className="account-panel">
-          <span>USERNAME</span>
-          <span>vilinhvu</span>
-          <span>EMAIL</span>
-          <span>vvkatt@gmail.com</span>
+          <div className="account-info">
+            <div className="account-field username">
+              <span className="label">USERNAME</span>
+              <span className="field">vilinhvu</span>
+            </div>
+            <div className="account-field email">
+              <span className="label">EMAIL</span>
+              <span className="field">vvkatt@gmail.com</span>
+            </div>
+            <div className="account-field birthday">
+              <span className="label">BIRTHDAY</span>
+              <span className="field">june 6, 2003</span>
+            </div>
+          </div>
         </div>
         <button className="edit">edit profile</button>
+
         <hr></hr>
         <h4>Password</h4>
         <button className="change-password">change password</button>
